ad736c24ecb208c4b4f1dd439750e3787b48cb3b
Gabriel Brunichaki <gabriel.brunichaki@triider.com>
2020-08-21 18:38:24 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function formatValue(value = 0) {
+  let [real, cents = 0] = value.toString().split('.');
+
+  real = real.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  cents = cents.toString().padEnd(2, '0');
+
+  return `R$ ${real},${cents}`;
+}
+
+module.exports = {
+  formatValue,
+};
